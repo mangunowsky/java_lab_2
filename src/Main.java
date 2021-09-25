83c1e99971af16e125fb20c9131df0e96e8d83ff
mangunowsky <michal.magun@icloud.com>
2021-09-25 12:21:12 +0200

    Add ćw1

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,22 @@ public class Main {
           to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
         */
 
+        /* Mój numer indexu 56571 */
+
+        int i = 1000;
+        int d = 71;
+
+        do{
+            if(i % d == 0){
+                System.out.printf("Liczba %d podzielna przez %d. \n", i, d);
+                i--;
+            }
+            else {
+//                System.out.println(i);
+                i--;
+            }
+        }
+        while (i >0);
 
         /* zad 2
         a) stworzyć tablicę typu int wielkosci 5, przypisac jej dowolne wartości,
@@ -17,10 +33,37 @@ public class Main {
         c) jak wyżej dla tablicy String[], przeiterować i wyświetlić od ostatniego elementu
         */
 
+        int y = 0;
+        int[] tabInt = {43, 123, 53, 454, 98};
+        double[] tabDouble = {123d, 7894127d, 87328d, 78617d, 1d};
+        String[] tabString = {"Wyższa", "Szkoła", "Bankowa", "w", "Gdańsku"};
+
+        while (y < tabInt.length){
+            System.out.println(tabInt[y]);
+            y++;
+        }
+
+        y = 0;
+        while (y < tabDouble.length){
+            System.out.println(tabDouble[y]);
+            y++;
+        }
+
+        y = 0;
+        while (y < tabString.length){
+            System.out.println(tabString[y]);
+            y++;
+        }
+
         /* zad.3
         a) sprawdzić jak zachowuje się pętla while gdy w jej warunku damy true --> while(true)
         b) a jak gdy damy false --> while(false)
         */
-
+        while(true){    //pętla będzie działać w nieskończoność
+            System.out.println("WHILE TRUE");
+        }
+        while(false){     //pętla nie wykona się ani razi, program się nie skompiluje
+            System.out.println("WHILE FALSE");
+        }
     }
 }
