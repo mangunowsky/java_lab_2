3378b3eede26d2374212e0b7c0f5cee766ea1378
mangunowsky <michal.magun@icloud.com>
2021-09-25 13:27:35 +0200

    Add ćw4

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,30 @@ public class Main {
         c) w trzeciej pętli return
         */
 
+        for(int x = 0; x < 10; x++){
+            if(x == 3){
+                break;
+            }
+            else{
+                System.out.println(x);
+            }
+        }
+        for(int i = 0; i < 10; i++){
+            if(i % 3 == 2){
+                continue;
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+        for(int i = 0; i < 10; i++){
+            if(i == 9){
+                return;
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+
     }
 }
